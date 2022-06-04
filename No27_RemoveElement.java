1748d581d9675a14a9d5dec98b47e0908a07b844
NanjanChung <derrick801127@gmail.com>
2022-06-04 22:47:42 +0800

    Update No27_RemoveElement.java

--- No27_RemoveElement.java
+++ No27_RemoveElement.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int numToRemove) {        
-        sortedEndIndex = 0;
+        int sortedEndIndex = 0;
         for(int i = 0; i<nums.length; i++)
         {
             if(nums[i]==numToRemove)
