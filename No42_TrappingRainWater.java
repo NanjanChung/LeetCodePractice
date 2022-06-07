b1969c1ff4b82ee9be2ca9327d65282025125204
NanjanChung <derrick801127@gmail.com>
2022-06-07 22:51:41 +0800

    Update No42_TrappingRainWater.java

--- No42_TrappingRainWater.java
+++ No42_TrappingRainWater.java
@@ -2,6 +2,26 @@
 
 class Solution {
     public int trap(int[] height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int i = 0;
+        int j = height.length - 1;
         
+        int trappedWater = 0;
+        while(i < j) {
+            leftMax = Math.max(leftMax, height[i]);
+            rightMax = Math.max(rightMax, height[j]);
+            if(leftMax < rightMax) {
+                trappedWater += leftMax - height[i];
+                i++;
+            }
+            else {
+                trappedWater += rightMax - height[j];
+                j--;
+            }   
+        }
+        return trappedWater;
     }
 }
+
+// require to write `Math` when using max or min
